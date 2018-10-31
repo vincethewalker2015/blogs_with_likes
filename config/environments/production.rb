8c61605c29738ad5e46d43f596f42bdd79e3e180
vincentbwalker <vincethewalker@gmail.com>
2018-10-31 17:51:35 +0000

    Configured Redis for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.web_socket_server_url = "wss://walkerville-in-realtime.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://walkerville-in-realtime.herokuapp.com', 'http://walkerville-in-realtime.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://walkerville-likes.herokuapp.com/', 'http://walkerville-likes.herokuapp.com/']
 end
